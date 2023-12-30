9f923f6394324140f8909d468065084873bc9ddc
yolayolayolanda <yolayolayolanda@foxmail.com>
2023-12-30 23:45:17 +0800

    SceneCut.py - try-catch & metadata recording method update

--- SceneCut.py
+++ SceneCut.py
@@ -37,59 +37,58 @@ def find_scenes(video_path, threshold=27.0):
     return scene_manager.get_scene_list()
 
 
-def main(vid_dir, out_dir, file_list, queue):
+def main(vid_dir, out_dir, file_list):
     threshold = 30.0
 
-    metas = []
-    for vid_file in tqdm(file_list):
-        if '.' in vid_file:
-            vid_name, vid_ext = vid_file.rsplit('.', 1)
-        else:
-            continue
-        if vid_ext in ['mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', 'mpeg', 'mpg']:
-            vid_path = os.path.join(vid_dir, vid_file)
-            scenes = find_scenes(vid_path, threshold=threshold)
-            for index, scene in enumerate(scenes):
-                metadata = {'basic': {}, 'scene': {}, 'camera': {}, 'misc': {}}
-                metadata['basic']['video_id'] = vid_name
-                metadata['basic']['video_path'] = os.path.join(os.path.basename(vid_dir), vid_file)
-                metadata['basic']['video_resolution'] = get_video_resolution(vid_path)
-                metadata['basic']['video_duration'] = scenes[-1][1].get_seconds()
-                metadata['basic']['video_fps'] = scenes[0][0].get_framerate()
-                metadata['basic']['clip_id'] = f'{vid_name}_{"%07d" % index}'
-                metadata['basic']['clip_path'] = f"{os.path.basename(out_dir)}/{metadata['basic']['clip_id']}.mp4"
-                metadata['basic']['clip_duration'] = (scene[1] - scene[0]).get_seconds()
-                metadata['basic']['clip_start_end_idx'] = [scene[0].get_frames(), scene[1].get_frames()]
-                metadata['basic']['optimal_score'] = None
-                split_video_ffmpeg(vid_path, [scene], f"{out_dir}/{metadata['basic']['clip_id']}.mp4")
+    with open(os.path.join(out_dir, 'metadata.txt'), 'a') as out_file:
+        for vid_file in tqdm(file_list):
+            try:
+                if '.' in vid_file:
+                    vid_name, vid_ext = vid_file.rsplit('.', 1)
+                else:
+                    continue
+                if vid_ext in ['mp4', 'avi', 'mkv', 'mov', 'wmv', 'flv', 'webm', 'mpeg', 'mpg']:
+                    vid_path = os.path.join(vid_dir, vid_file)
+                    scenes = find_scenes(vid_path, threshold=threshold)
+                    for index, scene in enumerate(scenes):
+                        metadata = {'basic': {}, 'scene': {}, 'camera': {}, 'misc': {}}
+                        metadata['basic']['video_id'] = vid_name
+                        metadata['basic']['video_path'] = os.path.join(os.path.basename(vid_dir), vid_file)
+                        metadata['basic']['video_resolution'] = get_video_resolution(vid_path)
+                        metadata['basic']['video_duration'] = scenes[-1][1].get_seconds()
+                        metadata['basic']['video_fps'] = scenes[0][0].get_framerate()
+                        metadata['basic']['clip_id'] = f'{vid_name}_{"%07d" % index}'
+                        metadata['basic']['clip_path'] = f"{os.path.basename(out_dir)}/{metadata['basic']['clip_id']}.mp4"
+                        metadata['basic']['clip_duration'] = (scene[1] - scene[0]).get_seconds()
+                        metadata['basic']['clip_start_end_idx'] = [scene[0].get_frames(), scene[1].get_frames()]
+                        metadata['basic']['optimal_score'] = None
+                        split_video_ffmpeg(vid_path, [scene], f"{out_dir}/{metadata['basic']['clip_id']}.mp4")
 
-                metas.append(metadata)
+                        output_metadata = json.dumps(metadata)
+                        out_file.write(output_metadata + '\n')
+            except Exception as e:
+                print("An error occurred:", str(e))
 
-    queue.put(metas)
 
 def run__process(vid_dir, out_dir, num_process):
     process = []
     results = []
-    queue = mp.Queue()
 
     os.makedirs(out_dir, exist_ok=True)
+    out_json_path = os.path.join(out_dir, 'metadata.txt')
     if not os.path.isabs(vid_dir):
         vid_dir = os.path.abspath(vid_dir)
+    with open(out_json_path, 'w') as oj:
+        pass
 
     file_list = os.listdir(vid_dir)
     chunk_size = len(file_list) // num_process if len(file_list) >= num_process else len(file_list)
     chunks = [file_list[i:i + chunk_size] for i in range(0, len(file_list), chunk_size)]
 
     for chunk in chunks:
-        process.append(mp.Process(target=main, args=(vid_dir, out_dir, chunk, queue)))
+        process.append(mp.Process(target=main, args=(vid_dir, out_dir, chunk)))
     [p.start() for p in process]  # 开启了len(threshold_list)个进程
     [p.join() for p in process]
-    for _ in process:
-        results.extend(queue.get())
-
-    out_json_path = os.path.join(out_dir, 'metadata.json')
-    with open(out_json_path, 'w+') as oj:
-        json.dump(results, oj)
 
 
 if __name__ == "__main__":
