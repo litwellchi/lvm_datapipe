233f37058d7071122dc83c9875a590529e3ec56d
Fang, Pengjun <138440893+ff2416@users.noreply.github.com>
2024-04-17 04:02:55 +0800

    Update ocr_score.py

--- evaluations/ocr_score.py
+++ evaluations/ocr_score.py
@@ -5,9 +5,8 @@ import glob
 import cv2
 import os
 import json
-from PIL import Image,ImageDraw
 import argparse
-
+from PIL import Image,ImageDraw
 
 def save_checkpoint(n,no):
     checkpoint = {
@@ -42,7 +41,6 @@ def get_frames(video_path,sample_rate):
     
     return frames,duration
 
-
 def calculate(frames):
     area=0
     for img in frames:
@@ -61,63 +59,57 @@ def calculate(frames):
                 x = [point[0] for point in box]
                 y = [point[1] for point in box]
                 area+= 0.5 * abs((x[0]*y[1] + x[1]*y[2] + x[2]*y[3] + x[3]*y[0]) - (y[0]*x[1] + y[1]*x[2] + y[2]*x[3] + y[3]*x[0]))  
-
-    return (area/1280/720/len(frames))
     
-def process(num,no,sample_rate,video_paths):
-    output_file=f'output_10frame{no}.json'
-    n=load_checkpoint(no)
+    w, h = frames[0].shape[1], frames[0].shape[0]
+    return (area/w/h/len(frames))
+    
+def process(args):
+    video_paths=[]
+    video_paths.extend(sorted(glob.glob(f"{args.vid_dir}/*")))
+    num=args.num_process
+    no=args.mp_no
+    sample_rate=args.sample_rate
+    output_file=f'{args.out_dir}ocr_score{no}.json'
+    n=load_checkpoint(no) 
 
-    with open(output_file, "a") as file:
-        if os.path.getsize(output_file) == 0:
-            file.write("[")
-        else:
-            file.seek(file.tell() - 1) 
+    if no!=num-1:
+        paths=video_paths[len(video_paths)//num*no+load_checkpoint(no):len(video_paths)//num*(no+1)]
+        length=len(video_paths)//num
+    else:
+        paths=video_paths[len(video_paths)//num*no+load_checkpoint(no):]
+        length=len(video_paths)-len(video_paths)//num*no
 
-        if no!=num-1:
-            paths=video_paths[len(video_paths)//num*no+load_checkpoint(no):len(video_paths)//num*(no+1)]
-            length=len(video_paths)//num
-        else:
-            paths=video_paths[len(video_paths)//num*no+load_checkpoint(no):]
-            length=len(video_paths)-len(video_paths)//num*no
+    for video in paths:
+        time_start=time.time()
+        frames,duration=get_frames(video,sample_rate)
+        score=calculate(frames)
 
-        for video in paths:
-            time_start=time.time()
-            frames,duration=get_frames(video,sample_rate)
-            score=calculate(frames)
-
-            result = {
-                        'id': os.path.splitext(os.path.basename(video))[0],
-                        'OCR_Score': score,
-                        'duration': duration,
-                        'time_taken': time.time()-time_start,
-                    }
-
+        result = {
+                    'id': os.path.splitext(os.path.basename(video))[0],
+                    'OCR_Score': score,
+                    'duration': duration,
+                    'time_taken': time.time()-time_start,
+                }
+        with open(output_file, "a") as file:
+            if os.path.getsize(output_file) == 0:
+                file.write("[")
             json.dump(result, file,indent=4)
             if n<length - 1:
                 file.write(",\n")
-            print(n)
-            n+=1
-            save_checkpoint(n,no)
+
+        save_checkpoint(n,no)
+    with open(output_file, "a") as file:
         file.write("]")
 
-
-
-if '__name__' == '__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_process', default=1, type=int)
-    parser.add_argument('--mp_no', default=0, type=int)
-    parser.add_argument('--sample_rate', default=10, type=int)
-    parser.add_argument('--vid_dir', deafault="/project/llmsvgen/share/data/macvid_4s/video_dataset_85", type=str)
-    parser.add_argument('--out_dir', type=str)
+    parser.add_argument("--vid_dir", type=str, default="/project/llmsvgen/share/data/macvid_4s/videos/video_dataset_85")
+    parser.add_argument("--num_process", type=int, default=1)
+    parser.add_argument("--mp_no", type=int, default=0)
+    parser.add_argument("--sample_rate", type=int, default=10)
+    parser.add_argument("--out_dir",type=str,default="./")
     args = parser.parse_args()
-    num=1
-    no=0
-    sample_rate=10
 
     reader = easyocr.Reader(['ch_sim','en']) # this needs to run only once to load the model into memory
-    folder_path = "/project/llmsvgen/share/data/macvid_4s/video_dataset_85"  
-    video_paths=[]
-
-    video_paths.extend(sorted(glob.glob(f"{folder_path}/*")))
-    process(num,no,sample_rate,video_paths)
+    
+    process(args)
