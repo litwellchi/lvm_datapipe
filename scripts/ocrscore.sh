d6be7f55d2307a18940514bec978d02e619fe51c
Fang, Pengjun <138440893+ff2416@users.noreply.github.com>
2024-04-17 14:05:54 +0800

    Adding OCR

--- scripts/ocrscore.sh
+++ scripts/ocrscore.sh
@@ -1,1 +1,11 @@
-pip install easyocr
+#pip install easyocr
+#pip install opencv-python
+python ocr_score.py \
+--vid_dir /project/llmsvgen/share/data/macvid_4s/videos \
+--metadata_path /project/llmsvgen/share/data/macvid_4s/metadata/all/macvid_4s_cp_0.json \
+--out_dir "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore" \
+--ckpt_path "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore/checkpoints" \
+--sample_rate 10 \
+--num_process 1 \
+--mp_no 0
+
