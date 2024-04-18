b9002b0e591dd7accf989057489435f53695a56c
Fang, Pengjun <138440893+ff2416@users.noreply.github.com>
2024-04-19 00:33:29 +0800

    refine code format

--- scripts/ocrscore.sh
+++ scripts/ocrscore.sh
@@ -3,8 +3,8 @@
 python ocr_score.py \
 --vid_dir /project/llmsvgen/share/data/macvid_4s/videos \
 --metadata_path /project/llmsvgen/share/data/macvid_4s/metadata/all/macvid_4s_cp_0.json \
---out_dir "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore" \
---ckpt_path "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore/checkpoints" \
+--out_dir "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore/macvid_4s_cp_0" \
+--ckpt_path "/project/llmsvgen/share/data/macvid_4s/metadata/ocrscore/macvid_4s_cp_0/checkpoints" \
 --sample_rate 10 \
 --num_process 1 \
 --mp_no 0
